626672891334513f6b9819aebae1ec274e673b52
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-07-02 14:06:43 +0400

    FIXED MIN SEC AND ADDDED RESet

--- src/app/components/stopwatch.tsx
+++ src/app/components/stopwatch.tsx
@@ -2,31 +2,39 @@ import { useEffect, useState } from "react";
 
 export default function stopwatch() {
 
-    const [time,SetTime] = useState(0)
-    const [mode,SetMode] = useState(false)
+    const [time, SetTime] = useState(0)
+    const [mode, SetMode] = useState(false)
 
-   useEffect(()=>{
+    useEffect(() => {
 
-    if(mode === false) return ;
+        if (mode === false) return;
 
-    const interval= setInterval(() => {
-        SetTime(prev => prev + 1)
-        
-    },1000);
+        const interval = setInterval(() => {
+            SetTime(prev => prev + 1)
+
+        }, 1000);
 
 
-return()=> clearInterval(interval);
+        return () => clearInterval(interval);
 
-    
 
-   },[mode]);
 
-    return(
+    }, [mode]);
+
+    const handelReset = () =>{
+        SetTime(0)
+    }
+
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+
+    return (
         <div>
-        <button
-        onClick={()=>{SetMode(!mode);}}
-        className={`border-2 text-black ${mode? "bg-red-700":"bg-green-300"}`}>{mode ? "Stop" : "Start"}</button>
-        <h1>{time}</h1>
+            <button
+                onClick={() => { SetMode(!mode); }}
+                className={`border-2 text-black ${mode ? "bg-red-700" : "bg-green-300"}`}>{mode ? "Stop" : "Start"}</button>
+            <h1>{min}:{sec}</h1>
+            <button onClick={handelReset}>RESET</button>
         </div>
     );
 }
